d61e339c33f818fafd3d0e972b8f30a718b2037b
Shaun Djuhari <shaundjuhari@berkeley.edu>
2016-09-06 23:07:15 -0700

    more functionality

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import {    FieldScrollerBehavior,    FieldLabelBehavior} from 'field';impo
 
 let whiteSkin = new Skin({    fill: "white",    borders: { left: 2, right: 2, top: 2, bottom: 2 },    stroke: "#df4747"});
 var hugeRedText = new Style({ font: "bold 20px", color: "red" });
+var blackText = new Style({font: "bold 16px", color:"black"})
 
 var StringTemplate = Label.template($ => ({    left: 0, right: 0, top: 0, bottom: 0,    style: $.style,    string: $.string}));
 
@@ -30,40 +31,48 @@ let MyRadioGroup = RadioGroup.template($ => ({    top: 0, bottom: 0, left: 0, r
             foodConverted = buttonName;
             trace("food Converted="+ foodConverted + "\n");        }    }}));
 
-var totalCalories= 0;
-var quantity = 2;
-let bigText = new Style({ font: "bold 25px", color: "#333333" });let MyButtonTemplate = Button.template($ => ({    top: 0, bottom: 0, left: 0, right: 0,    contents: [        Label($, {left: 0, right: 0, height: 55, string: $.textForLabel, style: bigText})    ],    Behavior: class extends ButtonBehavior {        onTap(button){            application.remove(mainCon);
 
-            totalCalories=quantity*listOfFoods[foodConverted];
-            var convString = countCalories(totalCalories);
+let bigText = new Style({ font: "bold 25px", color: "#333333" });let AgainButtonTemplate = Button.template($ => ({    top: 0, bottom: 0, left: 0, right: 0,    contents: [        Label($, {left: 0, right: 0, height: 25, string: $.textForLabel, style: bigText})    ],    Behavior: class extends ButtonBehavior {        onTap(button){          application.remove(secondCon);        	application.add(mainCon);        }    }}));var secondCon;var totalCalories= 0;var quantity = 2;let GoButtonTemplate = Button.template($ => ({    top: 0, bottom: 0, left: 0, right: 0,    contents: [        Label($, {left: 0, right: 0, height: 25, string: $.textForLabel, style: bigText})    ],    Behavior: class extends ButtonBehavior {        onTap(button){          application.remove(mainCon);
 
-            trace("output="+convString);
-            trace("food Converted="+ foodConverted + "\n");
+          totalCalories=quantity*listOfFoods[foodConverted];
+          var convString = countCalories(totalCalories);
 
-            var secondCon = new Column({			    left: 0, right: 0, top: 0, bottom: 0, skin: whiteSkin,			    contents: [
-			    	new StringTemplate({style: hugeRedText, string: "You consumed: "+totalCalories+" calories" }),			        new StringTemplate({ style: hugeRedText, string: convString}),			    ]			});        	application.add(secondCon);
+          trace("output="+convString);
+          trace("food Converted="+ foodConverted + "\n");
+
+          secondCon = new Column({			          left: 0, right: 0, top: 0, bottom: 0, skin: whiteSkin,			          contents: [
+                        new Column({
+                          left:0,right:0,top:0,bottom:0,skin:whiteSkin,
+                          contents:[new StringTemplate({style: hugeRedText, string: "You consumed: "+totalCalories+" calories" }),
+                                    new AgainButtonTemplate({textForLabel: "Again!"}),
+                                    new StringTemplate({style: hugeRedText, string: "The equivalent of:" }),
+                          ]
+                        }),
+                           new Line({left:0, right:0, top:0, bottom:0, skin:whiteSkin,                             contents:[ new Column({left:0,right:0,top:0,bottom:0,skin:whiteSkin,                                                  contents:[new StringTemplate({ style: blackText, string: convertedFoods["Plate of Spaghetti"]+" Plate of Spaghetti"}),                                                  new StringTemplate({ style: blackText, string: convertedFoods["Banana"]+" Banana"}),                                                  new StringTemplate({ style: blackText, string: convertedFoods["Pop Tart"]+" Pop Tart"}),                                                  new StringTemplate({ style: blackText, string: convertedFoods["Big Mac"]+" Big Mac"}),                                                  new StringTemplate({ style: blackText, string: convertedFoods["Medium Fries"]+" Medium Fries"}),                                                  new StringTemplate({ style: blackText, string: convertedFoods["Taco"]+" Taco"}),                                                  new StringTemplate({ style: blackText, string: convertedFoods["Slice of bread"]+" Slice of bread"})                                                  ]                                                }),                                        new Column({left:0,right:0,top:0,bottom:0,skin:whiteSkin,                                                  contents:[new StringTemplate({ style: blackText, string: convertedFoods["Chocolate Cake"]+" Chocolate Cake"}),                                                  new StringTemplate({ style: blackText, string: convertedFoods["Plate of Pad Thai"]+" Plate of Pad Thai"}),                                                  new StringTemplate({ style: blackText, string: convertedFoods["IHOP Chorizo Fiesta Omelette"]+" IHOP Chorizo Fiesta Omelette"}),                                                  new StringTemplate({ style: blackText, string: convertedFoods["Harmless Coconut Water (1 bottle)"]+" Harmless Coconut Water (1 bottle)"}),                                                  new StringTemplate({ style: blackText, string: convertedFoods["Boba milk tea with grass jelly 50% sugar"]+" Boba milk tea with grass jelly 50% sugar"}),                                                  new StringTemplate({ style: blackText, string: convertedFoods["Cup of black coffee"]+" Cup of black coffee"}),                                                  new StringTemplate({ style: blackText, string: convertedFoods["Grande Caramel Frappuccino"]+" Grande Caramel Frappuccino"}),                                                  ]                                                    })                             ]                           })			    ]			});        	application.add(secondCon);
         }    }}));
 
+var convertedFoods = listOfFoods;
 function countCalories(calories){
 	var output = ""
 	for (var food in listOfFoods){
 		if(listOfFoods.hasOwnProperty(food)){
-			output += ""+ Math.round(calories/listOfFoods[food] * 100) / 100 +" "+food + " "
+      var foodConverted = Math.round(calories/listOfFoods[food] * 100) / 100
+			output += ""+ foodConverted +" "+food + " "
+      convertedFoods[food]=foodConverted
 		}
 	}
 	return output
 }
 
-let nameInputSkin = new Skin({borders: {left: 2, right: 2, top: 2, bottom: 2 }, stroke: 'gray'});let fieldStyle = new Style({color: 'black', font: 'bold 24px', horizontal: 'left', vertical: 'middle', left: 5, right: 5, top: 5, bottom: 5});let fieldHintStyle = new Style({color: '#aaa', font: '24px', horizontal: 'left', vertical: 'middle', left: 5, right: 5, top: 5, bottom: 5});let fieldLabelSkin = new Skin({ fill: ['transparent', 'transparent', '#C0C0C0', '#acd473'] });let MyField = Container.template($ => ({    width: 200, height: 36, skin: nameInputSkin, contents: [        Scroller($, {            left: 4, right: 4, top: 4, bottom: 4, active: true,            Behavior: FieldScrollerBehavior, clip: true,            contents: [                Label($, {                    left: 0, top: 0, bottom: 0, skin: fieldLabelSkin,                    style: fieldStyle, anchor: 'NAME',                    editable: true, string: $.name,                    Behavior: class extends FieldLabelBehavior {                        onEdited(label) {                            let data = this.data;                            data.name = label.string;                            label.container.hint.visible = (data.name.length == 0);                            quantity = parseInt(data.name)                            trace(data.name+"\n");                        }                    },                }),                Label($, {                    left: 4, right: 4, top: 4, bottom: 4, style: fieldHintStyle,                    string: "Tap to add quantity...", name: "hint"                }),            ]        })    ]}));
+let nameInputSkin = new Skin({borders: {left: 2, right: 2, top: 2, bottom: 2 }, stroke: 'gray'});let fieldStyle = new Style({color: 'black', font: 'bold 20px', horizontal: 'left', vertical: 'middle', left: 5, right: 5, top: 5, bottom: 5});let fieldHintStyle = new Style({color: '#aaa', font: '20px', horizontal: 'left', vertical: 'middle', left: 5, right: 5, top: 5, bottom: 5});let fieldLabelSkin = new Skin({ fill: ['transparent', 'transparent', '#C0C0C0', '#acd473'] });let MyField = Container.template($ => ({    width: 200, height: 36, skin: nameInputSkin, contents: [        Scroller($, {            left: 4, right: 4, top: 4, bottom: 4, active: true,            Behavior: FieldScrollerBehavior, clip: true,            contents: [                Label($, {                    left: 0, top: 0, bottom: 0, skin: fieldLabelSkin,                    style: fieldStyle, anchor: 'NAME',                    editable: true, string: $.name,                    Behavior: class extends FieldLabelBehavior {                        onEdited(label) {                            let data = this.data;                            data.name = label.string;                            label.container.hint.visible = (data.name.length == 0);                            quantity = parseInt(data.name)                            trace(data.name+"\n");                        }                    },                }),                Label($, {                    left: 4, right: 4, top: 4, bottom: 4, style: fieldHintStyle,                    string: "Tap to add quantity", name: "hint"                }),            ]        })    ]}));
 
 var mainCon = new Column({    left: 0, right: 0, top: 0, bottom: 0, skin: whiteSkin,    contents: [
     	new Line({left: 0, right: 0, top: 0, bottom: 0, skin: whiteSkin,
     		contents: [
-	    		new StringTemplate({ style: hugeRedText,	            string: "Quantity?" }),
-	            new MyField({name: ""}),
+	    		    new StringTemplate({ style: hugeRedText, string: "Quantity?" }),	            new MyField({name: ""}),
 	            ]
     	}),
-        new MyRadioGroup({buttonNames: "Plate of Spaghetti,Banana,Big Mac,Slice of Bread,Plate of Pad Thai"}),        new MyButtonTemplate({ textForLabel: "Go!" }),    ]});
+        new MyRadioGroup({buttonNames: "Plate of Spaghetti,Banana,Big Mac,Slice of Bread,Plate of Pad Thai,Boba milk tea with grass jelly 50% sugar,Grande Caramel Frappuccino"}),        new GoButtonTemplate({ textForLabel: "Go!" }),    ]});
 
 
 
